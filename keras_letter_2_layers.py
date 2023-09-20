54e0c69415cb3ab5697a508c877e0e46cd9e44a9
NoeDrk <130477810+NoeDrk@users.noreply.github.com>
2023-09-20 15:36:08 +0200

    Update keras_letter_2_layers.py

--- keras_letter_2_layers.py
+++ keras_letter_2_layers.py
@@ -58,7 +58,7 @@ en_train = []
 
 
 #Import fr/en word pairs
-with open("/Users/noederijck/Desktop/word_lists/fr_en_long_word_list.txt", "r") as file:
+with open("fr_en_long_word_list.txt", "r") as file:
     for line in file:
         word_pair = line.strip().split(",")
         words_list.append(word_pair)
